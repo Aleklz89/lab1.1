eb1de6764c9f2fa75819efd8c093163148a8da5f
AleksandrovIA-03 <ia03.aleksandrov.oleg@gmail.com>
2023-01-17 01:06:40 +0200

    [METHOD] POST DELETE PATCH OPTIONS

--- src/router.js
+++ src/router.js
@@ -2,6 +2,10 @@ class Router {
   constructor() {
     this.routes = new Map();
     this.get = this.addHandler.bind(this, 'GET')
+    this.post = this.addHandler.bind(this, 'POST')
+    this.patch = this.addHandler.bind(this, 'PATCH')
+    this.delete = this.addHandler.bind(this, 'DELETE')
+    this.options = this.addHandler.bind(this, 'OPTIONS')
   }
 
   addHandler(method, path, handler) {
